cd7fba970642c518eb9324e6928516df0d0af020
Winters67 <emmanuel.vbg@gmail.com>
2022-12-31 14:33:43 +0100

    Modification des cards

---
+++ scripts/card.js
@@ -0,0 +1,68 @@
+export { showCard };
+
+function showCard(recette) {
+  const clock = `/assets/icons/clock-regular.svg`;
+  const picture = `/assets/images/wendy.jpg`;
+  const recetteCard = document.createElement("div");
+  recetteCard.setAttribute("class", "recette card");
+  const imgCard = document.createElement("img");
+  imgCard.setAttribute("class", "image card-img-top");
+  imgCard.setAttribute("src", picture);
+  const descriptionCard = document.createElement("div");
+  descriptionCard.setAttribute("class", "description card-body bg-light");
+  const cardTitle = document.createElement("div");
+  cardTitle.classList.add("card-title");
+  const recetteName = document.createElement("h5");
+  const cardTime = document.createElement("span");
+  cardTime.setAttribute("class", "card-time");
+  const recetteTime = document.createElement("img");
+  recetteTime.setAttribute("class", "m-1");
+  recetteTime.setAttribute("style", "width: 18px; height: 18px");
+  recetteTime.setAttribute("src", clock);
+  const recetteTimeDesc = document.createElement("h4");
+  recetteName.textContent = recette.name;
+  recetteTimeDesc.textContent = recette.time + " min";
+  const cardIngredients = document.createElement("article");
+  cardIngredients.setAttribute("id", "card-ingredients");
+  cardIngredients.setAttribute("class", "card-recette");
+  const descriptionRecette = document.createElement("p");
+  descriptionRecette.setAttribute("class", "card-text w-50");
+  descriptionRecette.textContent = recette.description;
+
+  const listIngredients = document.createElement("div");
+  listIngredients.setAttribute('style', 'padding-right:10px')
+  for (const recetteIng of recette.ingredients) {
+    let div = document.createElement("div");
+    div.setAttribute("class", "card-ingredients w-25");
+    
+    let ingredient = document.createElement("p");
+    ingredient.setAttribute("style", "font-weight: 700");
+    ingredient.textContent = recetteIng.ingredient +":"; 
+    
+    let quantity = document.createElement("p");
+    quantity.textContent = recetteIng.quantity;
+
+    let unit = document.createElement("p");
+    unit.setAttribute("class", "unity");
+    unit.textContent = recetteIng.unit;
+
+
+    listIngredients.appendChild(div);
+    div.appendChild(ingredient);
+    div.appendChild(quantity);
+    div.appendChild(unit);
+  }
+
+  recetteCard.appendChild(imgCard);
+  recetteCard.appendChild(descriptionCard);
+  descriptionCard.appendChild(cardTitle);
+  descriptionCard.appendChild(cardIngredients);
+  cardIngredients.appendChild(listIngredients);
+  cardIngredients.appendChild(descriptionRecette);
+  cardTitle.appendChild(recetteName);
+  cardTitle.appendChild(cardTime);
+  cardTime.appendChild(recetteTime);
+  cardTime.appendChild(recetteTimeDesc);
+
+  return recetteCard;
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,49 +1,44 @@
 import { recipes } from "../data/recipes.js";
-console.log(recipes);
+import { showCard } from "../scripts/card.js";
+// console.log(recipes);
 
-for (let index = 0; index < recipes.length; index++) {
-  const recette = recipes[index];
+const tags = {
+  ingredients: [],
+  appliances: [],
+};
+
+//Boucle
+//Retrouve les ingredients : tags.ingredients.push(ingredient)
+
+// ecouteur input
+const inputField = document.getElementById("search-input");
+inputField.addEventListener("input", () => {
+  console.log(inputField.value);
+  //If inputField.value > 3
+  // Traitement du tableau (filtre les recipes)
+  // Condition valuer.input / recette.include(valuer.input)
+});
+
+// boucle recette
+recipes.forEach((recette) => {
   console.log(recette);
 
-  for (let index = 0; index < recette.ingredients.length; index++) {
-    const ingredients = recette.ingredients[index];
-    console.log(ingredients);
-  }
+  // let listElement = recette.ingredients.map((recette) => recette);
+ 
 
-  function showCard() {
-    document.querySelector("#card-item").innerHTML += `
-        
-        <div class="card m-2" style="width: 380px; height: 364px">
-        <img
-        class="card-img-top"
-        src="/assets/images/wendy.jpg"
-        alt="Card image cap"
-        />
-        <div class="card-body bg-light">
-        <div class="card-title">
-        <p>${recette.name}</p>
-        <span class="card-time">
-        <img
-        class="m-1"
-        src="/assets/icons/clock.png"
-        alt="horloge"
-        style="width: 18px; height: 18px"
-        />
-        <p>${recette.time}</p>
-        </span>
-        </div>
-        <article class="card-recette">
-        <p class="card-ingredients w-50">
-        ${recette.ingredient}
-        </p>
-        <p class="card-text w-50">
-        ${recette.description}
-        </p>
-        </article>
-        </div>
-     
-        </div>`;
-  }
 
-  showCard();
-}
+  const listeAppareils = recette.appliance;
+  console.log(listeAppareils);
+
+  const listeUstensiles = recette.ustensils;
+  console.log(listeUstensiles);
+
+  // affichage des cartes
+  const displayCard = document.querySelector("#card-item")
+  displayCard.appendChild(showCard(recette));
+
+
+});
+
+console.log(showCard)
+
